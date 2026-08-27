10bdb4c74b024d2f513a966b9d013457d7ff1ab9
Herval Mata <hervalmataamparo@gmail.com>
2026-08-27 17:57:55 -0300

    :sparkles: feat: new ecommerce frontend - setup products query and show

--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -4,5 +4,6 @@ const router = require("express").Router();
 router.get('/get-categories', homeController.get_categories)
 router.get('/get-products', homeController.get_Products)
 router.get('/price-range-latest-product', homeController.price_range_product)
+router.get('/query_products', homeController.query_products)
 
 module.exports = router
